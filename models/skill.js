fc3fb46fae9386028f7b68f00d8cb079f45d93c3
federalolive <j3pumphrey@gmail.com>
2020-12-03 20:43:36 -0600

    Add dir mdels and touch skill.js. Add data to skill.js

---
+++ models/skill.js
@@ -0,0 +1,24 @@
+const skills = [
+    {skill: 'Troubleshooting', proficiency: true},
+    {skill: 'Javascript', proficiency: true},
+    {skill: 'CSS', proficiency: true},
+    {skill: 'C++', proficiency: true},
+    {skill: 'C#', proficiency: true},
+    {skill: 'Python', proficiency: true},
+    {skill: 'Ruby on Rails', proficiency: true},
+    {skill: 'React', proficiency: true},
+    {skill: 'ASP.NET', proficiency: true},
+    {skill: 'C', proficiency: true},
+    {skill: 'Perl', proficiency: true},
+    {skill: 'Java', proficiency: true},
+    {skill: 'SQL', proficiency: true},
+]
+
+module.exports = {
+    getAll
+}
+
+function getAll() {
+    return skills;
+}
+
